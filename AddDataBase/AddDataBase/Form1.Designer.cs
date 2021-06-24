afa3917b3b3e0acd075601d957cf8a241c3350ba
DangNguyen146 <19521317@gm.uit.edu.vn>
2021-06-24 21:10:10 +0700

    database

---
+++ AddDataBase/AddDataBase/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace AddDataBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.maVan = new System.Windows.Forms.TextBox();
+            this.tenVan = new System.Windows.Forms.TextBox();
+            this.giaVan = new System.Windows.Forms.TextBox();
+            this.hinhAnh = new System.Windows.Forms.TextBox();
+            this.danhGia = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.moTa = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "maVan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "tenVan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(272, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "hinhAnh";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(272, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "giaVan";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(522, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "danhGia";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(522, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "moTa";
+            // 
+            // maVan
+            // 
+            this.maVan.Location = new System.Drawing.Point(96, 25);
+            this.maVan.Name = "maVan";
+            this.maVan.Size = new System.Drawing.Size(175, 20);
+            this.maVan.TabIndex = 6;
+            // 
+            // tenVan
+            // 
+            this.tenVan.Location = new System.Drawing.Point(96, 53);
+            this.tenVan.Name = "tenVan";
+            this.tenVan.Size = new System.Drawing.Size(175, 20);
+            this.tenVan.TabIndex = 7;
+            // 
+            // giaVan
+            // 
+            this.giaVan.Location = new System.Drawing.Point(341, 26);
+            this.giaVan.Name = "giaVan";
+            this.giaVan.Size = new System.Drawing.Size(175, 20);
+            this.giaVan.TabIndex = 8;
+            // 
+            // hinhAnh
+            // 
+            this.hinhAnh.Location = new System.Drawing.Point(341, 53);
+            this.hinhAnh.Name = "hinhAnh";
+            this.hinhAnh.Size = new System.Drawing.Size(175, 20);
+            this.hinhAnh.TabIndex = 9;
+            // 
+            // danhGia
+            // 
+            this.danhGia.Location = new System.Drawing.Point(590, 53);
+            this.danhGia.Name = "danhGia";
+            this.danhGia.Size = new System.Drawing.Size(175, 20);
+            this.danhGia.TabIndex = 11;
+            this.danhGia.Text = "5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 130);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 308);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // moTa
+            // 
+            this.moTa.Enabled = false;
+            this.moTa.Location = new System.Drawing.Point(590, 26);
+            this.moTa.Name = "moTa";
+            this.moTa.Size = new System.Drawing.Size(175, 20);
+            this.moTa.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.danhGia);
+            this.Controls.Add(this.moTa);
+            this.Controls.Add(this.hinhAnh);
+            this.Controls.Add(this.giaVan);
+            this.Controls.Add(this.tenVan);
+            this.Controls.Add(this.maVan);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox maVan;
+        private System.Windows.Forms.TextBox tenVan;
+        private System.Windows.Forms.TextBox giaVan;
+        private System.Windows.Forms.TextBox hinhAnh;
+        private System.Windows.Forms.TextBox danhGia;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox moTa;
+    }
+}
+
---
+++ AddDataBase/AddDataBase/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddDataBase
+{
+    public partial class Form1 : Form
+    {
+        int index = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            maVan.Text = index++.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            maVan.Text = index++.ToString();
+            richTextBox1.Text += ",\n"+"{\n";
+            richTextBox1.Text += '"' + "maVan" + '"' + ":" + maVan.Text + "," + "\n";
+            richTextBox1.Text += '"' + "tenVan" + '"' + ":" + '"' + tenVan.Text + '"' + "," + "\n";
+            richTextBox1.Text += '"' + "giaVan" + '"' + ":" + '"' + giaVan.Text + '"' + "," + "\n";
+            richTextBox1.Text += '"' + "hinhAnh" + '"' + ": " + '"' + "https://bizweb.dktcdn.net/thumb/medium/100/345/407/products/"  + hinhAnh.Text + '"' + "," + "\n";
+            richTextBox1.Text += '"' + "moTa" + '"' + ":" + '"' + tenVan.Text + '"' + "," + "\n";
+            richTextBox1.Text += '"' + "danhGia" + '"' + ":" + danhGia.Text + "\n";
+            richTextBox1.Text += "}";
+            giaVan.Text = "";
+            tenVan.Text = "";
+            hinhAnh.Text = "";
+        }
+    }
+}
